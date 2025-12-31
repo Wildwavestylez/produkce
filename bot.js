bd95d349ed5a8b243c1e730d0c906411a4622787
Wildwavestylez <140261374+Wildwavestylez@users.noreply.github.com>
2025-12-31 12:13:57 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,16 @@
 const TelegramBot = require("node-telegram-bot-api");
 
+// tvůj token a chat ID
 const BOT_TOKEN = "8030561920:AAG4-f_SWSNQQ8O11CjnhsJdEUnmVZev7k";
 const CHAT_ID = -5154393188;
 
-const bot = new TelegramBot(BOT_TOKEN, { polling: false });
+// vytvoření bota s bezpečnějším timeoutem
+const bot = new TelegramBot(BOT_TOKEN, {
+  polling: false,
+  request: { timeout: 10000 }
+});
 
+// odeslání zprávy
 bot.sendMessage(CHAT_ID, "Bot je naživu. GitHub zije.")
   .then(() => {
     console.log("Zprava odeslana");
